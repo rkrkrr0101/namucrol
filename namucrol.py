90817491e3d6bb1ef65f16b3358de8574cb3c73a
rkrkrr0101 <qkrrudgh0@naver.com>
2022-06-07 17:07:28 +0900

    0607

--- namucrol.py
+++ namucrol.py
@@ -115,7 +115,7 @@ db.commit()
 
 #한달치 데이터 긁어오기
 sql="""select * from namu_log
-order by na_time DESC LIMIT 8640
+order by na_time DESC LIMIT 4320
 """
 cursor.execute(sql)
 logres=cursor.fetchall()
@@ -142,7 +142,7 @@ values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
 """
 #count=[3,4,5,12]
 #여기적힌거갯수일때 작동
-count=[12,288,2016,8640]
+count=[6,144,1008,4320]
 countkind=['hour','day','week','month']
 
 #1시간 하루 이렇게 갯수세서 캐시에 넣기
